41fc4a61db362291a8e99515cb6612fb7fadec14
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-25 08:16:53 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,9 @@
+class  Conditional
+{
+	public static void main(String[] args) 
+	{   
+		int age=16;
+		String result = (age>=18);? "You are eligible for vote" : "You are not eligible for voting";
+		System.out.println(result);
+	}
+}
