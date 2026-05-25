08470185d82006f932c07deb87e0cd9576aaa35d
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-25 08:17:11 +0530

    Add files via upload

---
+++ Conditionalex.java
@@ -0,0 +1,9 @@
+class  ConditionalEx  
+{
+	public static void main(String[] args) 
+	{   
+		int age=16;
+		String result = (age>=18);? "You are eligible for vote" : "You are not eligible for voting";
+		System.out.println(result);
+	}
+}
