3d757bcad7fd2e679d7cecd8a7f9a7499b72cdb4
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-26 08:17:10 +0530

    Add files via upload

---
+++ EvenOddNumbers.java
@@ -0,0 +1,9 @@
+class  EvenOddNumbers 
+{
+	public static void main(String[] args) 
+	{   int n = 11;
+	    String result = (n%2==0) ? "Even number":"odd number";
+		System.out.println(n+":"+result);
+	}
+}
+// WAPTP largest of the given three numbers using the 
