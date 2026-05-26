e2e6b37a0717a0ded8a75442ea7a9c292040c96d
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-26 08:12:46 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+class  Demo1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(2+3);
+		System.out.println(2-3);
+		System.out.println(2*3);
+		System.out.println(17/5);
+		System.out.println(17%5);
+		System.out.println(17.0/5);
+		System.out.println(17.0%5);
+
+  }
+}
