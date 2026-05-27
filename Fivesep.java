f331e2aeaf890677c731bbbcba4e392c7341128a
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-27 13:55:05 +0530

    Add files via upload

---
+++ Fivesep.java
@@ -0,0 +1,17 @@
+class  Fivesep
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(10+20);
+		System.out.println('A'+10);
+		System.out.println('A'+'B');
+		//System.out.println(true+10); error
+	    System.out.println("hello"+10);
+		System.out.println("hello"+10+20);
+		System.out.println("hello"(+10+20);
+		System.out.println(10+20+"hello");
+		System.out.println("hello"+"everyone");
+		System.out.println("user age is 32");
+	/*this is concitanation in this we will be understood about the  arithmatic operations 
+	}
+}
