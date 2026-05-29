a9029b86a399d5ca4f6bb03c7ff23d55bff2f18a
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-29 14:00:52 +0530

    Add files via upload

---
+++ LoopExample3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class  LoopExample3
+{
+	public static void main(String[] args) 
+	{   Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x value:");
+		int x = sc.nextInt();
+		System.out.println("Enter Y value:");
+		int y = sc.nextInt();
+		for (int i=x;i<=y ;i++ )
+		{
+			System.out.println(i); 
+		}
+	}
+}
