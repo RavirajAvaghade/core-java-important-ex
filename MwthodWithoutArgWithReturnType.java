e05049dfd02a3c4cc2415cf33975474ac2f72065
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-01 14:01:03 +0530

    Add files via upload

---
+++ MwthodWithoutArgWithReturnType.java
@@ -0,0 +1,26 @@
+class  MwthodWithoutArgWithReturnType
+{
+	public static void main(String[] args) 
+	{
+		if (getpi()==3.1416)
+		{
+			System.out.print("true value");
+		}
+		else
+		{
+			System.out.print("false value");
+
+		}
+		
+
+	}
+	
+	
+	public static double getpi()
+	{
+		return 3.1416;
+
+	}
+	
+
+}
