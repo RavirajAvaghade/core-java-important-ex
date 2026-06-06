d88365fa1b1437a05394bb9fea7ef3f79772163d
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-06 14:32:53 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,17 @@
+class Pattern25 
+{
+	public static void main(String[] args) 
+	{
+
+		int star =5;
+		for (int i=1;i<=5;i++)
+		{
+			for (int a=1;a<=star ;a++)
+			{
+				System.out.print(" * ");
+			}
+			star--;
+			System.out.println();
+		}
+	}
+}
