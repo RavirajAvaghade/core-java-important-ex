bb078c8fb24a33b0db648c39cca060b62fb76dbf
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-08 14:20:27 +0530

    Add files via upload

---
+++ PrimeNumberEx2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class  PrimeNumberEx2
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		boolean flag = true;
+		for (int i =2;i<n;i++)
+		{
+			if (n%i==0)
+			{
+				flag = false;
+				break;
+			}
+		}
+		if (flag==true)
+		{
+			System.out.println("prime Number");
+		}
+		else
+		{
+			System.out.println("Not prime number:");
+
+		}
+	}
+}
