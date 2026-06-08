1b59206c8f1d8d2b65fbb9b3b2a73b1f81abd862
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-08 14:16:25 +0530

    Add files via upload

---
+++ PracticeEx.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class PracticeEx
+{ 
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a X value:");
+    	int x = sc.nextInt();
+		System.out.println("Enter a Y value:");
+		int y = sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=n;i++)
+		{
+			sum=sum+i;
+		
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
