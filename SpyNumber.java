d87df9f8d2e489deec512c7e8c7fb1981f07ffab
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-09 14:01:26 +0530

    Add files via upload

---
+++ SpyNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class  SpyNumber
+{
+	public static void main(String[] args) 
+	{   Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int num = sc.nextInt();
+		int sum = 0;
+		int product = 1;
+		while (num!=0)
+		{
+			int ld = num%10;
+			sum = sum+ld;
+			product = product*ld;
+			num = num/10;
+	    }
+		if (sum==product)
+		{
+			System.out.println("spy number");
+		}
+		else
+		{
+			System.out.println("Not spy");
+
+		}
+	}
+}
