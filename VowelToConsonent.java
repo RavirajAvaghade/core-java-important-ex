8cb0689949f54c00708f08f35023d3a821639a18
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-11 12:52:20 +0530

    Add files via upload

---
+++ VowelToConsonent.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class  VowelToConsonent
+{
+	public static void main(String[] args) 
+	{   Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the alphabet:");
+		char ch = sc.next().charAt(0);
+		if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println(ch+"is a vowel");
+		}
+		else if (ch>='a' && ch<='z')
+		{
+			System.out.println(ch+"is consonant");
+		}
+		else
+		{
+			System.out.println("Not an alphabet");
+		}
+	}
+}
