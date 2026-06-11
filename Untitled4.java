f99f5624ca982bad6d4d5d7854c6eb9badef20b0
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-06-11 12:49:36 +0530

    Add files via upload

---
+++ Untitled4.java
@@ -0,0 +1,12 @@
+class  And_operation  
+{
+	public static void main(String[] args) 
+	{   int age=19;
+	if (age>=21&&age<=32);
+	{   System.out.println("You are eligible");
+
+	}
+	else
+		System.out.println("You are not eligible);
+	}
+}
